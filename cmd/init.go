464b1b0ec8c400b38cd0f5858fa2c4a005f1734e
myferr <contactme.myfer@protonmail.com>
2025-08-18 19:44:12 -0500

    feat(init): add pie.yml file creation to init command

--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/spf13/cobra"
 )
@@ -9,9 +10,25 @@ import (
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initializes a new Pie project.",
-	Long:  `Creates a pie.yml file and a .pie/Piefile to configure the Python environment.`,
+	Long:  `Creates a pie.yml file to configure the Python environment.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Initializing Pie project...")
+
+		// Create pie.yml
+		pieYmlContent := `main: main.py
+python: "3.12"
+# You can specify a dependency file, or list your dependencies here
+# deps_file: PieDeps.yaml
+dependencies:
+  - pandas
+  - numpy
+`
+		err := ioutil.WriteFile("pie.yml", []byte(pieYmlContent), 0644)
+		if err != nil {
+			fmt.Println("Error creating pie.yml:", err)
+			return
+		}
+		fmt.Println("Created pie.yml")
 	},
 }
 
